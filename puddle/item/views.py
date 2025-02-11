9663d51be13dad0e265d29172df11f4497abcc53
Uel-5 <kariukiwaweru8156@gmail.com>
2025-02-11 10:43:48 +0300

    related items

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -4,8 +4,10 @@ from .models import Item
 # return 404 if item's details  isn't available. 
 def detail(request,pk): #pk==primary-key
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
 
     return render (request, "item/detail.html",{
-        "item": item
+        "item": item,
+        "related_items":related_items
     })
 
