806bd21ceb74c2eb37539441727b9b99139277d7
Uel-5 <kariukiwaweru8156@gmail.com>
2025-02-11 12:14:04 +0300

    `Added signup view and updated URLs, templates and views to reflect changes`

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,12 +1,10 @@
-from core.views import contact, index
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',index,name='index'),
+    path('',include("core.urls")),
     path("items/", include("item.urls")),
-    path("contact/",contact,name="contact"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # image 
